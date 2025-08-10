926747909b6d2c0d54352fb7b9cd48b732e48c86
DEAN Cacioppo <dean@oneclick.agency>
2025-08-09 20:36:36 -0500

    fix: Render thank you page correctly

---
+++ thank-you-entry.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ThankYou from './components/ThankYou';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThankYou />
+  </React.StrictMode>,
+);
